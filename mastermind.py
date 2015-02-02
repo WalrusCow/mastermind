964d06571bb407dd65c8b8027e77ab888ebf8ea6
WalrusCow <willgmcd@gmail.com>
2015-02-01 22:55:36 -0800

    Mastermind game

---
+++ mastermind.py
@@ -0,0 +1,33 @@
+from collections import Counter
+from random import randrange
+
+class Game:
+    def __init__(self, maxNum=9, numSecrets=4):
+        self._numSecrets = numSecrets
+        self._maxNum = maxNum
+        self._secret = self._genSecret()
+
+    def _genSecret(self):
+        return [randrange(0, self._maxNum + 1)
+                for _ in range(self._numSecrets + 1)]
+
+    def guess(self, guess):
+        # Returns (exact, inexact)
+        exact = 0
+        secret = self._secret[::]
+        for i, (g, s) in enumerate(zip(guess[::], self._secret)):
+            if g != s: continue
+            # Well, we are kinda modifying lists as we go through them
+            # which is generally a bad idea...
+            secret.pop(i - exact)
+            guess.pop(i - exact)
+            exact += 1
+
+        secretCount = Counter(secret)
+
+        inexact = 0
+        for g in guess:
+            if secretCount[g] > 0:
+                secretCount[g] -= 1
+                inexact += 1
+        return exact, inexact
