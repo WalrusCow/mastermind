e506fbfa3a9f769aeb062206d213702db1ccf021
WalrusCow <willgmcd@gmail.com>
2015-02-01 23:40:25 -0800

    More useful game

--- mastermind.py
+++ mastermind.py
@@ -2,20 +2,28 @@ from collections import Counter
 from random import randrange
 
 class Game:
-    def __init__(self, maxNum=9, numSecrets=4):
+    def __init__(self, maxNum=9, numSecrets=4, secret=None):
         self._numSecrets = numSecrets
         self._maxNum = maxNum
-        self._secret = self._genSecret()
+        if secret is None:
+            self._secret = self._genSecret()
+        else:
+            self._secret = list(secret)
 
     def _genSecret(self):
-        return [randrange(0, self._maxNum + 1)
-                for _ in range(self._numSecrets + 1)]
+        return [randrange(0, self._maxNum + 1) for _ in range(self._numSecrets)]
 
-    def guess(self, guess):
+    def unwrapInt(self, num):
+        return list(map(int, str(num).rjust(self._numSecrets, '0')))
+
+    @staticmethod
+    def _guess(secret, guess):
         # Returns (exact, inexact)
+        guess = list(guess)
+        secret = list(secret)
+
         exact = 0
-        secret = self._secret[::]
-        for i, (g, s) in enumerate(zip(guess[::], self._secret)):
+        for i, (g, s) in enumerate(zip(guess[::], secret[::])):
             if g != s: continue
             # Well, we are kinda modifying lists as we go through them
             # which is generally a bad idea...
@@ -28,3 +36,7 @@ class Game:
 
         inexact = sum(min(guessCount[k], secretCount[k]) for k in guessCount)
         return exact, inexact
+
+    def guess(self, guess):
+        # Returns (exact, inexact)
+        return Game._guess(self._secret, guess)
